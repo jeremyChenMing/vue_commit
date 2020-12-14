395026b629613ab647e526fb06b7236443c71d82
chenming3 <361196961@qq.com>
2020-12-14 11:28:01 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
+<!-- vt -->
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="about">about {{ value }}</div>
 </template>
+
+<script>
+//import name from 'path';
+
+export default {
+  components: {},
+  data() {
+    return {
+      value: 1
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="less" scoped>
+//@import url(); 引入公共css类
+.about {
+  width: 100%;
+}
+</style>
